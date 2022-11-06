5f585b5c15f01d54820026f930f1a7ee9560055a
Leapofod <ittihadi28@gmail.com>
2022-11-07 06:23:36 +0700

    Push Project Files

---
+++ FasterUI.cs
@@ -0,0 +1,13 @@
+using Terraria.ModLoader;
+
+namespace FasterUI;
+
+public class FasterUI : Mod
+{
+	internal static float CraftScrollMultiplier => ModContent.GetInstance<FasterUIConfig>().CraftingScrollMultiplier;
+
+	public override void Load()
+	{
+		InventoryCraftingEdits.ApplyInventoryCraftEdit();
+	}
+}
